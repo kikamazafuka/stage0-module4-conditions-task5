402739933a9a1b866f83aba4560023d0a8a68a7b
Artur Semenas <uncelfuka@gmail.com>
2023-11-09 23:58:45 +0100

    Module #4.1. Task #5. Switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        System.out.println(year%400==0 || year%4==0 && year%100!=0 ?"leap":"not leap");
+
     }
 }
